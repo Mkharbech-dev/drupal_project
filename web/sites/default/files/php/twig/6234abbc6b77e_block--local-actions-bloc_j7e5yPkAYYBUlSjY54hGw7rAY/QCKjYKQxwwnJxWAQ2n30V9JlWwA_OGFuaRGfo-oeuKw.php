4bf70f6584131f68e94158f5d1c9c207a44e1c61
Mkharbech salah eddine <salaheddine.mk8@gmail.com>
2022-03-18 17:04:01 +0100

    les themes de couleur

---
+++ web/sites/default/files/php/twig/6234abbc6b77e_block--local-actions-bloc_j7e5yPkAYYBUlSjY54hGw7rAY/QCKjYKQxwwnJxWAQ2n30V9JlWwA_OGFuaRGfo-oeuKw.php
@@ -0,0 +1,119 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* core/themes/seven/templates/block--local-actions-block.html.twig */
+class __TwigTemplate_4294c979266c4e32648daf3d8e5ce8c30b0f456a436839f3dec3e0816e26ef1c extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+            'content' => [$this, 'block_content'],
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "
+";
+        // line 8
+        $this->displayBlock('content', $context, $blocks);
+    }
+
+    public function block_content($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 9
+        echo "  ";
+        if (($context["content"] ?? null)) {
+            // line 10
+            echo "    <ul class=\"action-links\">
+      ";
+            // line 11
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["content"] ?? null), 11, $this->source), "html", null, true);
+            echo "
+    </ul>
+  ";
+        }
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/seven/templates/block--local-actions-block.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  56 => 11,  53 => 10,  50 => 9,  43 => 8,  40 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("
+{#
+/**
+ * @file
+ * Theme override for local actions (primary admin actions.)
+ */
+#}
+{% block content %}
+  {% if content %}
+    <ul class=\"action-links\">
+      {{ content }}
+    </ul>
+  {% endif %}
+{% endblock %}
+", "core/themes/seven/templates/block--local-actions-block.html.twig", "C:\\wamp64\\www\\recettes\\web\\core\\themes\\seven\\templates\\block--local-actions-block.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("block" => 8, "if" => 9);
+        static $filters = array("escape" => 11);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['block', 'if'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
